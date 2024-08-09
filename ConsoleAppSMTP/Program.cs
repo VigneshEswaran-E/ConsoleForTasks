2177f4bc43e9a31470d785d2e5df75a4945deff4
vasanthvishnu <vasanthraj.selva@gmail.com>
2024-08-09 13:36:04 +0530

    json email

--- ConsoleAppSMTP/Program.cs
+++ ConsoleAppSMTP/Program.cs
@@ -4,20 +4,36 @@ using System.Net.Mail;
 using TaskSMTP;
 using TaskReadAndWrite;
 using Read_Date_From_JSON_File;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
 namespace ConsoleAppSMTP
 {
     class Program
     {
        static void Main (string[] args)
         {
-            ReadDate obj = new ReadDate();
-            obj.DI();
-            
+
+           var serviceCollection = new ServiceCollection();
+            IConfiguration configuration;
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsetting.Json")
+                .Build();
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+            Smtp obj = new Smtp(configuration);
+            obj.send();
+           
+            // ReadDate obj = new ReadDate();
+            //obj.DI();
+
             // ReadAndWrite data = new ReadAndWrite();
             //data.File();
 
-           // smtp obj = new ();
-           // obj.send();
+            // Smtp obj = new Smtp ();
+         //  obj.send();
         }
         
     }
--- SMTPProgram/smtp.cs
+++ SMTPProgram/smtp.cs
@@ -1,19 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace TaskSMTP
+{
+   public class Smtp
     {
+        private readonly IConfiguration configuration;
+        
+
+     
+        public Smtp(IConfiguration configuration)
+        {
+
+            this.configuration = configuration;
+        }
+
+        public void FileLog()
+        {
+            string file = $"File_{DateTime.Now.ToString("yyyy-MM-dd")}.txt";
+            try
+            {
+                send();
+                StreamWriter sw = new StreamWriter($"D:{file}.txt", false);
+
+                sw.WriteLine($"Successfully sent Mail in{DateTime.Now.ToString("yyyy-MM-dd")}");
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                StreamWriter sw1 = new StreamWriter($"D:{file}.txt", false);
+                sw1.WriteLine(e.StackTrace);
+                sw1.Close();
+            }
+        }
         public void send()
         {
             Console.WriteLine("Hello World!");
             SendEmail(GetUserName(), GetPassword());
-            Console.ReadLine();
+
+
         }
 
-        public static void SendEmail(string fromAddress, string password)
+        public void SendEmail(string fromAddress, string password)
         {
             using SmtpClient email = new SmtpClient
             {
@@ -22,39 +57,38 @@ using System.Threading.Tasks;
                 EnableSsl = true,
                 Host = "smtp.gmail.com",
                 Port = 587,
-                Credentials = new NetworkCredential(fromAddress, password)
-            };
+                Credentials = new NetworkCredential(userName: fromAddress, password),
 
-            string subjet = "Reminder";
-            string body = $"hi sir i am  vignesh @ {DateTime.UtcNow:F}";
+            };
+            string subject = "youtube";
+            string body = $"this is main email @{DateTime.UtcNow:F}";
             try
             {
-                Console.WriteLine("sending email ********");
-                email.Send(fromAddress, ToAddress(), subjet, body);
-                Console.WriteLine("email sent **********");
+                Console.WriteLine("sending email ");
+                email.Send(fromAddress, ToAddress(), subject, body);
+                Console.WriteLine("email sent ");
             }
             catch (SmtpException e)
             {
-                Console.WriteLine(e);
-
+                Console.WriteLine("email sent");
             }
 
-
         }
-        public static string GetUserName()
+        public string GetUserName()
         {
-            return "eswaranevignesh@gmail.com";
+            var dataFromJsonFile = configuration.GetSection("From").Value;
+            return dataFromJsonFile;
 
         }
         public static string GetPassword()
         {
-            return "tgbapdjspgklhziz";
+            return "nzkrwtkssqzcuwal";
         }
-
-
-        public static string ToAddress()
+        public string ToAddress()
         {
-            return "sureshkumar.duraisamy@anaiyaantechnologies.com";
+            var dataFromJsonFile1 = configuration.GetSection("To").Value;
+            return dataFromJsonFile1;
+            
         }
     }
 }
